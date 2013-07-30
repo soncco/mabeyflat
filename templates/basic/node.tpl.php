2f6df4c04eff51357f3aec66ebcbdc0dde471b8a
brau <soncco@gmail.com>
2013-07-29 23:37:11 -0500

    Overriding template files

---
+++ templates/basic/node.tpl.php
@@ -0,0 +1,27 @@
+<article>
+
+  <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
+    <header>
+
+      <?php print render($title_prefix); ?>
+      <?php if (!$page && $title): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($unpublished): ?>
+        <p class="unpublished"><?php print t('Unpublished'); ?></p>
+      <?php endif; ?>
+    </header>
+  <?php endif; ?>
+
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    print render($content);
+  ?>
+
+  <?php print render($content['links']); ?>
+
+</article><!-- /.node -->
