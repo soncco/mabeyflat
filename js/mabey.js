a9ea946d420c44005063c8b55db6cb17d459fc08
brau <soncco@gmail.com>
2013-07-29 23:36:38 -0500

    Pure forms

--- js/mabey.js
+++ js/mabey.js
@@ -2,12 +2,33 @@
   Drupal.behaviors.mabeyflat = {
     attach: function (context) {
       
+      // Responsive Slider.      
       $('.bxslider', context).bxSlider({
         pager: false,
         captions: true
       });
 
+      // Scroll to top.
       $().UItoTop({ easingType: 'easeOutQuart' });
+
+      // Forms.
+      $('form').addClass('pure-form pure-form-aligned');
+      $('.form-item').addClass('pure-control-group');
+      $('.form-type-checkbox').removeClass('pure-control-group').addClass('pure-controls');
+      $('.form-actions').addClass('pure-controls');
+      $('.form-actions input[type=submit]').addClass('pure-button pure-button-primary');
+
+      $textarea = $('.form-textarea-wrapper textarea').clone();
+      $('.form-textarea-wrapper').remove();
+      $textarea.appendTo('.form-type-textarea');
+      $textarea.appendTo('.webform-component-textarea');
+
+      $('.element-invisible').remove();
+      $selects = $('.webform-container-inline select');
+      $('.webform-component-date').append($selects);
+      $wfci = $('.webform-container-inline');
+      $wfci.remove();
+
     }
   };
 })(jQuery);
